4529994a698a750976683f00993c621ae7b5bf17
David Rueda <davd33@gmail.com>
2021-04-11 04:07:44 +0200

    console clean

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import axios from 'axios';
 import Peer from 'simple-peer';
 import RStream from 'readable-stream';
+import {VideoCanva} from './components/video-canva/VideoCanva';
 import './App.css';
 
 function App(){
 
 
     const wsConnect = (isStreamMode) => {
-        console.log('connect to python websocket');
         const socket = new window.WebSocket(`ws://${window.token}:8769`);
 
         socket.addEventListener('message', onMessage);
@@ -25,38 +25,29 @@ function App(){
         let peer;
 
         function onMessage (event) {
-            console.log('0');
             const message = event.data;
-            console.log(message);
+
             if (JSON.parse(message) === 'ready') {
-                console.log('2', peer);
                 if (peer) return;
                 peer = new Peer({ initiator: isStreamMode });
-                console.log('3', peer);
                 peer.on('signal', function (signal) {
-                    console.log('signal !');
                     socket.send(JSON.stringify(signal));
                 });
                 if (isStreamMode) {
-                    console.log('5');
                     peer.on('connect', function () {
-                        console.log('stream try to send');
                         endless.pipe(peer);
                         console.log("stream mode - send endless");
                     });
                 } else {
-                    console.log('6');
                     peer.on('data', function (message) {
                         console.log('receive mode', message.length);
                     });
                 }
 
             } else if (peer) {
-                console.log('not ready');
                 peer.signal(JSON.parse(message));
             }
 
-            console.log(event);
         }
     };
 
